3e94dbad0e163a15543b8bf7c376d2911c2e3a0c
shingchi <shingchi@sina.cn>
2016-04-27 19:35:12 +0800

    修改默认端口为SSL模式下的

--- Plugin.php
+++ Plugin.php
@@ -47,7 +47,7 @@ class Passport_Plugin implements Typecho_Plugin_Interface
     public static function config(Typecho_Widget_Helper_Form $form){
 
         $host = new Typecho_Widget_Helper_Form_Element_Text('host', NULL, '', _t('服务器(SMTP)'), _t('如: smtp.exmail.qq.com'));
-        $port = new Typecho_Widget_Helper_Form_Element_Text('port', NULL, '25', _t('端口'), _t('如: 25、465(SSL)、587(SSL)'));
+        $port = new Typecho_Widget_Helper_Form_Element_Text('port', NULL, '465', _t('端口'), _t('如: 25、465(SSL)、587(SSL)'));
 
         $username = new Typecho_Widget_Helper_Form_Element_Text('username', NULL, '', _t('帐号'), _t('如: hello@example.com'));
         $password = new Typecho_Widget_Helper_Form_Element_Password('password', NULL, NULL, _t('密码'));
